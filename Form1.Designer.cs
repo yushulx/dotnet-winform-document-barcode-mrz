1b3ce8e779113e0735bc5320c2d18dd0c8c4da72
yushulx <lingxiao1002@gmail.com>
2023-07-27 15:18:16 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.enterLicenseKeyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.menuStrip2.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 24);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 769);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.AllowDrop = true;
+            this.button1.AutoEllipsis = true;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(1218, 24);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(280, 74);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AllowDrop = true;
+            this.button2.AutoEllipsis = true;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(1218, 106);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(280, 74);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Camera Scan";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(613, 24);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(600, 769);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(1218, 221);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(280, 190);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Tag = "";
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1218, 194);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 15);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "MRZ Information";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 24);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1507, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.enterLicenseKeyToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(1507, 24);
+            this.menuStrip2.TabIndex = 11;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // enterLicenseKeyToolStripMenuItem
+            // 
+            this.enterLicenseKeyToolStripMenuItem.Name = "enterLicenseKeyToolStripMenuItem";
+            this.enterLicenseKeyToolStripMenuItem.Size = new System.Drawing.Size(110, 20);
+            this.enterLicenseKeyToolStripMenuItem.Text = "Enter License Key";
+            this.enterLicenseKeyToolStripMenuItem.Click += new System.EventHandler(this.enterLicenseKeyToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 797);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1507, 22);
+            this.statusStrip1.TabIndex = 12;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(1219, 456);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(279, 334);
+            this.listBox1.TabIndex = 14;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+            this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox1_DragEnter);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1218, 426);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 15);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Image Files";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1507, 819);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = ".NET Dynamsoft Capture Vision Demo";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private PictureBox pictureBox2;
+        private RichTextBox richTextBox1;
+        private Label label1;
+        private MenuStrip menuStrip1;
+        private MenuStrip menuStrip2;
+        private ToolStripMenuItem enterLicenseKeyToolStripMenuItem;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        private ListBox listBox1;
+        private Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,247 @@
+﻿using System.Drawing.Imaging;
+using Dynamsoft;
+using Result = Dynamsoft.MrzScanner.Result;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using Point = OpenCvSharp.Point;
+using static Dynamsoft.MrzScanner;
+using System.Text.Json.Nodes;
+using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        private MrzScanner scanner;
+        private VideoCapture capture;
+        private bool isCapturing;
+        private Thread? thread;
+        private Mat _mat = new Mat();
+        private Result[]? _results;
+
+        public Form1()
+        {
+            InitializeComponent();
+            FormClosing += new FormClosingEventHandler(Form1_Closing);
+            string license = "DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==";
+
+            ActivateLicense(license);
+
+            scanner = MrzScanner.Create();
+            capture = new VideoCapture(0);
+            isCapturing = false;
+            scanner.LoadModel();
+        }
+        
+        private void ActivateLicense(string license)
+        {
+            int ret = MrzScanner.InitLicense(license); // Get a license key from https://www.dynamsoft.com/customer/license/trialLicense?product=dlr
+            if (ret != 0) 
+            {
+                toolStripStatusLabel1.Text = "License is invalid.";
+            }
+            else {
+                toolStripStatusLabel1.Text = "License is activated successfully.";
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            // Code
+            scanner.Destroy();
+        }
+
+        private void ShowResults(Result[] results)
+        {
+            if (results == null)
+                return;
+        }
+        private Bitmap DecodeMat(Mat mat)
+        {
+            int length = mat.Cols * mat.Rows * mat.ElemSize();
+            byte[] bytes = new byte[length];
+            Marshal.Copy(mat.Data, bytes, 0, length);
+            _results = scanner.DetectBuffer(bytes, mat.Cols, mat.Rows, (int)mat.Step(), MrzScanner.ImagePixelFormat.IPF_RGB_888);
+            if (_results != null)
+            {
+                string[] lines = new string[_results.Length];
+                var index = 0;
+                foreach (Result result in _results)
+                {
+                    lines[index++] = result.Text;
+                    richTextBox1.Text += result.Text + Environment.NewLine;
+                    if (result.Points != null)
+                    {
+                        Point[] points = new Point[4];
+                        for (int i = 0; i < 4; i++)
+                        {
+                            points[i] = new Point(result.Points[i * 2], result.Points[i * 2 + 1]);
+                        }
+                        Cv2.DrawContours(mat, new Point[][] { points }, 0, Scalar.Red, 2);
+                    }
+                }
+
+                JsonNode? info = Parse(lines);
+                if (info != null) richTextBox1.Text = info.ToString();
+            }
+
+            Bitmap bitmap = BitmapConverter.ToBitmap(mat);
+            return bitmap;
+        }
+
+        private void MrzDetection(string filename)
+        {
+            try
+            {
+                _mat = Cv2.ImRead(filename, ImreadModes.Color);
+                Mat copy = new Mat(_mat.Rows, _mat.Cols, MatType.CV_8UC3);
+                _mat.CopyTo(copy);
+                pictureBox1.Image = BitmapConverter.ToBitmap(copy);
+                pictureBox2.Image = DecodeMat(copy);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StopScan();
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Open Image";
+                dlg.Filter = "Image files (*.bmp, *.jpg, *.png) | *.bmp; *.jpg; *.png";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    listBox1.Items.Add(dlg.FileName);
+                    MrzDetection(dlg.FileName);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!capture.IsOpened())
+            {
+                MessageBox.Show("Failed to open video stream or file");
+                return;
+            }
+
+            if (button2.Text == "Camera Scan")
+            {
+                StartScan();
+            }
+            else
+            {
+                StopScan();
+            }
+        }
+
+        private void StartScan()
+        {
+            button2.Text = "Stop";
+            isCapturing = true;
+            thread = new Thread(new ThreadStart(FrameCallback));
+            thread.Start();
+        }
+
+        private void StopScan()
+        {
+            button2.Text = "Camera Scan";
+            isCapturing = false;
+            if (thread != null) thread.Join();
+        }
+
+        private void FrameCallback()
+        {
+            while (isCapturing)
+            {
+                capture.Read(_mat);
+                Mat copy = new Mat(_mat.Rows, _mat.Cols, MatType.CV_8UC3);
+                _mat.CopyTo(copy);
+                pictureBox1.Image = DecodeMat(copy);
+            }
+        }
+
+        private void Form1_Closing(object? sender, FormClosingEventArgs e)
+        {
+            StopScan();
+        }
+
+        private void enterLicenseKeyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string license = InputBox("Enter License Key", "", "");
+            if (license != null && license != "")
+            {
+                ActivateLicense(license);
+            }
+        }
+
+        public static string InputBox(string title, string promptText, string value)
+        {
+            Form form = new Form();
+            TextBox textBox = new TextBox();
+            Button buttonOk = new Button();
+            Button buttonCancel = new Button();
+
+            form.Text = title;
+            textBox.Text = value;
+
+            buttonOk.Text = "OK";
+            buttonCancel.Text = "Cancel";
+            buttonOk.DialogResult = DialogResult.OK;
+            buttonCancel.DialogResult = DialogResult.Cancel;
+
+            textBox.SetBounds(12, 36, 372, 20);
+            buttonOk.SetBounds(60, 72, 80, 30);
+            buttonCancel.SetBounds(260, 72, 80, 30);
+
+            form.ClientSize = new System.Drawing.Size(400, 120);
+            form.Controls.AddRange(new Control[] { textBox, buttonOk, buttonCancel });
+            form.FormBorderStyle = FormBorderStyle.FixedDialog;
+            form.StartPosition = FormStartPosition.CenterScreen;
+            form.MinimizeBox = false;
+            form.MaximizeBox = false;
+            form.AcceptButton = buttonOk;
+            form.CancelButton = buttonCancel;
+
+            DialogResult dialogResult = form.ShowDialog();
+            return textBox.Text;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string? filename = listBox1.SelectedItem.ToString();
+            if (filename != null)
+            {
+                MrzDetection(filename);
+            }
+        }
+
+        private void listBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data != null && e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void listBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data != null && e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                foreach (string file in files)
+                {
+                    listBox1.Items.Add(file);
+                }
+            }
+        }
+    }
+}
